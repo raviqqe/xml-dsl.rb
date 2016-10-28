3f748e05cc70af7adb33d11e12d2ecd3dff4acb8
Yota Toyama <raviqqe@gmail.com>
2016-10-28 13:39:03 +0900

    Modify compiler

--- lib/xml-dsl/compile.rb
+++ lib/xml-dsl/compile.rb
@@ -1,19 +1,36 @@
+require 'nokogiri'
+
+
+
 module XML
   def self.from_array array
-    array.map do |entry|
-      [entry[0].to_s.upcase, *entry[1..-1].map { |o| compile_object o }] \
-          .join(' ') + "\n"
+    Nokogiri::XML(compile_elems(array), &:noblanks)
+        .to_xml(indent: 4, &:no_declaration)
+  end
+
+  def self.compile_elems elems
+    elems.map do |elem|
+      tagged elem[0], elem[1..-2], elem[-1]
     end.join
   end
 
-  private_class_method def self.compile_object object
-    case object
-    when Array
-      '[' + object.map{ |o| o.to_s.dump }.join(', ') + ']'
-    when -> (o) { o.respond_to? :to_s }
-      object.to_s
-    else
-      raise "Invalid object is detected.: #{object}"
-    end
+  def self.tagged tag, attrs, elems
+    p "TAGGED"
+    p tag, attrs, elems
+
+    tag = tag.to_s
+    brackets(tag + ' ' + compile_attrs(attrs)) \
+        + (elems.is_a?(String) ? elems : compile_elems(elems)) \
+        + brackets('/' + tag)
+  end
+
+  def self.compile_attrs attrs
+    attrs.map do |name, value|
+      "#{name}=#{value.gsub('\\', '\\\\').gsub('"', '\\"')}"
+    end.join ' '
+  end
+
+  def self.brackets tag
+    '<' + tag + '>'
   end
 end
